6a72562aa3cac8394b7edcc1e912226b29e6f1bd
robpSF <robert@tstoryteller.com>
2024-08-09 05:42:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def infer_personality_and_write_bio(api_key, elements):
     prompt += "##STEP 2\n"
     prompt += "Write their twitter bio. \n\n"
     prompt += "##STEP 3\n"
-    prompt += "Write an instagram post based on the image. \n\n"
+    prompt += "Write an instagram post based on these image elememts:" + elements +"\n\n"
     prompt += "##RULES\n"
     prompt += "1. Don't use emojis\n"
     prompt += "2. Do NOT directly mention what's labelled, work from the personality\n\n"
