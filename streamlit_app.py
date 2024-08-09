3a008fc6575354b5208160a529705da3db51a2c5
robpSF <robert@tstoryteller.com>
2024-08-09 05:32:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ st.write('Upload the URL of an image to analyze and generate a Twitter bio.')
 
 # Retrieve secrets from Streamlit
 google_api_key_json = json.loads(st.secrets["json_key"])
-st.write("Raw JSON string from secrets:", google_api_key_json_str)
+st.write("Raw JSON string from secrets:", google_api_key_json)
 openai_api_key = st.secrets["api_key"]
 
 # Input field for image URL
